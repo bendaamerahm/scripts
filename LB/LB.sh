07b6587678c5050b1b0b926a2c3f7fd8f2eb5c39
bendaamerahm <ahmed.b.daamer@gmail.com>
2023-07-16 23:04:54 +0200

    update --remove action
    Signed-off-by: bendaamerahm <ahmed.b.daamer@gmail.com>

--- LB/LB.sh
+++ LB/LB.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Default configuration parameters
 VIP=""
 PORT=""
@@ -14,6 +16,7 @@ function display_help {
   echo "  -v, --vip <IP>         Specify the Virtual IP address (VIP)"
   echo "  -p, --port <PORT>      Specify the port number"
   echo "  -b, --backends <IPs>   Specify the backend server IP addresses, separated by spaces"
+  echo "      --remove           Remove load balancing rules"
   echo
 }
 
@@ -61,22 +64,28 @@ function set_up_load_balancing {
 
 # Remove load balancing rules and restore from backup
 function remove_load_balancing {
-  # Remove the load balancing rules
-  sudo iptables -t nat -F LB_CHAIN
-  sudo iptables -t nat -X LB_CHAIN
-  sudo iptables -t nat -D POSTROUTING -p tcp --dport $PORT -j MASQUERADE
-  sudo iptables -t nat -D PREROUTING -p tcp --dport $PORT -j DNAT --to-destination $VIP:$PORT
-  sudo iptables -t nat -D PREROUTING -p tcp -m tcp -d $VIP --dport $PORT -j REDIRECT --to-ports $PORT
+  # Prompt for confirmation
+  read -p "Are you sure you want to remove the load balancing rules? [y/N]: " choice
+  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    # Remove the load balancing rules
+    sudo iptables -t nat -F LB_CHAIN
+    sudo iptables -t nat -X LB_CHAIN
+    sudo iptables -t nat -D POSTROUTING -p tcp --dport $PORT -j MASQUERADE
+    sudo iptables -t nat -D PREROUTING -p tcp --dport $PORT -j DNAT --to-destination $VIP:$PORT
+    sudo iptables -t nat -D PREROUTING -p tcp -m tcp -d $VIP --dport $PORT -j REDIRECT --to-ports $PORT
 
-  echo "Load balancing rules removed."
+    echo "Load balancing rules removed."
 
-  # Restore iptables configuration from backup
-  if [ -f "iptables_backup_*" ]; then
-    echo "Restoring iptables configuration from backup..."
-    sudo iptables-restore < $(ls -t iptables_backup_* | head -1)
-    echo "iptables configuration restored from backup."
+    # Restore iptables configuration from backup
+    if [ -f "iptables_backup_*" ]; then
+      echo "Restoring iptables configuration from backup..."
+      sudo iptables-restore < $(ls -t iptables_backup_* | head -1)
+      echo "iptables configuration restored from backup."
+    else
+      echo "No iptables backup found."
+    fi
   else
-    echo "No iptables backup found."
+    echo "Load balancing removal aborted."
   fi
 }
 
@@ -100,6 +109,10 @@ function process_args {
         BACKENDS=($2)
         shift 2
         ;;
+      --remove|rm)
+        remove_load_balancing
+        exit 0
+        ;;
       *)
         echo "Invalid option: $1"
         display_help
@@ -111,6 +124,12 @@ function process_args {
 
 # Main script logic
 function main {
+  # If no options provided, display the help menu
+  if [[ $# -eq 0 ]]; then
+    display_help
+    exit 0
+  fi
+
   # Process command line arguments
   process_args "$@"
 
@@ -119,12 +138,6 @@ function main {
 
   # Set up load balancing
   set_up_load_balancing
-
-  # Prompt for rollback option
-  read -p "Do you want to remove load balancing rules? [y/N]: " rollback_choice
-  if [[ $rollback_choice == "y" || $rollback_choice == "Y" ]]; then
-    remove_load_balancing
-  fi
 }
 
 # Execute the script
